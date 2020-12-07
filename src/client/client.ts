240b23561ad003c2c19beaaefee31803c7875db3
Leonardo Camargo <leoofficevps@gmail.com>
2020-12-07 04:55:12 -0300

    add: client

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,14 +1,43 @@
 import { PluginManager } from '../managers/plugin/plugin-manager';
+import { Client as DiscordClient } from 'discord.js';
 import { PhilosophersPlugin } from '../plugins/philosopers/philosophers-plugin';
 
+interface IClientData {
+    token: string;
+}
+
 class Client {
+    token: string;
+    client: DiscordClient;
+
     plugins = new PluginManager();
 
-    public connect() {
-        this.plugins.add('philosophers', new PhilosophersPlugin());
+    constructor({ token }: IClientData) {
+        this.token = token;
+        this.client = new DiscordClient();
     }
 
-    public init() {}
+    public connect() {
+        this.client.login(this.token);
+        this.client.on('ready', () => {
+            this.client.user.setPresence({
+                activity: {
+                    name: '!jurandir help',
+                    type: 'PLAYING',
+                },
+                status: 'online',
+            });
+        });
+        this.init();
+    }
+
+    public init() {
+        this.client.on('message', message => {
+            this.plugins.emit('message', message);
+        });
+
+        this.plugins.add('philosophers', new PhilosophersPlugin());
+    }
 }
 
 export { Client };
