864767d343d228ea7bda82dc2468830cf8815e86
Leonardo Camargo <leoofficevps@gmail.com>
2020-12-07 04:53:20 -0300

    fix: parse correct message

--- src/command/command-parser.ts
+++ src/command/command-parser.ts
@@ -2,16 +2,21 @@ import { IParsedCommand } from '../managers/command/base-command';
 
 class CommandParser {
     public static parse(command: string): IParsedCommand {
-        const parts = command.split(' ');
+        let parts = command.split(' ');
 
         if (parts.length < 1) {
             return;
         }
 
         const key = parts[0];
+        parts.splice(0, 1);
+        const commandWithoutPreffix = parts.join(' ');
+
         const params = [];
 
-        for (let i = 1; i < parts.length; i++) {
+        parts = commandWithoutPreffix.split(', ');
+
+        for (let i = 0; i < parts.length; i++) {
             const part = parts[i];
 
             params.push(part);
